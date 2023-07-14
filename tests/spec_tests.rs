4d4bb333c98aa9a8f6d5e1efdbbfd4f66259a6a5
Alan Smith <commit@alanwsmith.com>
2023-07-14 19:43:30 -0400

    Setup an aside test and updated the structure to setup for text to produce a documentation page as well.

--- tests/spec_tests.rs
+++ tests/spec_tests.rs
@@ -1,5 +1,4 @@
 #![allow(unused_imports)]
-// use neopolengine::neo_section::neo_section;
 use neopolengine::neo_sections::neo_sections;
 use neopolengine::neo_sections::Section;
 use serde::{Deserialize, Serialize};
@@ -8,21 +7,33 @@ use serde_json::Value;
 use std::fs;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-struct SectionTestCase {
-    expected: Vec<Section>,
-    ignore: Option<bool>,
+struct TestParts {
     input: String,
+    expected: Vec<Section>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct SectionTestCase {
+    ignore: Option<bool>,
     solo: Option<bool>,
+    name: String,
+    notes: Vec<String>,
+    parts: TestParts,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct TestShell {
+    spec_tests: Vec<SectionTestCase>,
 }
 
 #[test]
 fn solo_test_specs() {
     let json_text = fs::read_to_string("./tests/neopolitan_spec_tests.json").unwrap();
-    let section_test_cases: Vec<SectionTestCase> =
-        serde_json::from_str(json_text.as_str()).unwrap();
+    let section_test_cases: TestShell = serde_json::from_str(json_text.as_str()).unwrap();
 
     // do solo tests first
     section_test_cases
+        .spec_tests
         .iter()
         .filter(|t| match t.solo {
             Some(just_me) if just_me == true => true,
@@ -30,15 +41,16 @@ fn solo_test_specs() {
         })
         .into_iter()
         .for_each(|x| {
-            dbg!(&x.input);
-            let results = neo_sections(&x.input).unwrap().1;
-            assert_eq!(x.expected, results);
+            dbg!(&x.parts.input);
+            let results = neo_sections(&x.parts.input).unwrap().1;
+            assert_eq!(x.parts.expected, results);
             ()
         });
 
     // then loop through everything that's not ignored
     // (this redoes the solo tests, but that's fine for now)
     section_test_cases
+        .spec_tests
         .iter()
         .filter(|t| match t.ignore {
             Some(skip_me) if skip_me == true => false,
@@ -46,9 +58,11 @@ fn solo_test_specs() {
         })
         .into_iter()
         .for_each(|x| {
-            dbg!(&x.input);
-            let results = neo_sections(&x.input).unwrap().1;
-            assert_eq!(x.expected, results);
+            dbg!(&x.parts.input);
+            let results = neo_sections(&x.parts.input).unwrap().1;
+            assert_eq!(x.parts.expected, results);
             ()
         });
+
+    //
 }
