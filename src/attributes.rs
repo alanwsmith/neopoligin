22b48848cd87b54af1709811abe7c018e90967a5
Alan Smith <commit@alanwsmith.com>
2023-07-18 19:32:10 -0400

    Startint to add attributes

--- src/attributes.rs
+++ src/attributes.rs
@@ -1,7 +1,155 @@
 use serde::{Deserialize, Serialize};
 
+
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 #[serde(tag = "type", content = "content", rename_all = "lowercase")]
 pub enum Attribute {
+    // these are global
     AccessKey(String),
+    AutoCapitalize(AutoCapitalizeAttrOption),
+    AutoFocus(),
+    Class(Vec<String>),
+    ContentEditable(ContentEditableAttrOption),
+    Data(String, String),
+    Dir(DirAttrOption),
+    Draggable(bool),
+    EnterKeyHint(String),
+    Hidden(Option<HiddenAttrOption>),
+    Id(String),
+    Inert(),
+    InputMode(InputModeAttrOption),
+    Is(String),
+    ItemId(String),
+    ItemProp(String),
+    ItemRef(String),
+    ItemScope(String),
+    ItemType(String),
+    Lang(String),
+    Nonce(String),
+    Part(Vec<String>),
+    Popover(PopoverAttrOption),
+    PopoverTarget(String),
+    Spellcheck(bool),
+    Style(Vec<(String, String)>),
+    TabIndex(i32), // can be negative
+    Title(String),
+    Translate(TranslateAttrOption),
+    VirtualKeyboardPolicy(VirtualKeyboardPolicyAttrOption),
+    // these are specific
+    Accept(),
+    AutoComplete(),
+    Capture(),
+    CrossOrigin(),
+    Disabled(),
+    ElementTiming(),
+    For(),
+    Max(),
+    MaxLength(),
+    Min(),
+    MinLength(),
+    Multiple(),
+    Pattern(),
+    ReadOnly(),
+    Rel(),
+    Required(),
+    Size(),
+    Step(),
+    None, // <- for dev testing
+}
+
+
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[serde(tag = "type", content = "content", rename_all = "lowercase")]
+pub enum AutoCapitalizeAttrOption {
+    Off(),
+    None(),
+    On(),
+    Sentences(),
+    Words(),
+    Characters(),
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[serde(tag = "type", content = "content", rename_all = "lowercase")]
+pub enum ContentEditableAttrOption {
+    True(),
+    False(),
+    PlaintextOnly(),
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[serde(tag = "type", content = "content", rename_all = "lowercase")]
+pub enum DirAttrOption {
+    Ltr(),
+    Rtl(),
+    Auto(),
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[serde(tag = "type", content = "content", rename_all = "lowercase")]
+pub enum HiddenAttrOption {
+    Hidden(),
+    UntilFound(),
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[serde(tag = "type", content = "content", rename_all = "lowercase")]
+pub enum InputModeAttrOption {
+    None(),
+    Text(),
+    Decimal(),
+    Numeric(),
+    Tel(),
+    Search(),
+    Email(),
+    Url(),
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[serde(tag = "type", content = "content", rename_all = "lowercase")]
+pub enum PopoverAttrOption {
+    Auto(),
+    Manual(),
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[serde(tag = "type", content = "content", rename_all = "lowercase")]
+pub enum TranslateAttrOption {
+    Yes(),
+    No(),
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[serde(tag = "type", content = "content", rename_all = "lowercase")]
+pub enum VirtualKeyboardPolicyAttrOption {
+    Auto(),
+    Manual(),
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[serde(tag = "type", content = "content", rename_all = "lowercase")]
+pub enum InputTypes {
+    Button(),
+    Checkbox(),
+    Color(),
+    Date(),
+    DatatimeLocal(),
+    Email(),
+    File(),
+    Hidden(),
+    Image(),
+    Month(),
+    Number(),
+    Password(),
+    Radio(),
+    Range(),
+    Reset(),
+    Search(),
+    Submit(),
+    Tel(),
+    Text(),
+    Time(),
+    Url(),
+    Week(),
 }
--- src/neo_sections/aside.rs
+++ src/neo_sections/aside.rs
@@ -1,3 +1,4 @@
+use crate::attributes::Attribute;
 use crate::neo_sections::Block;
 use crate::neo_sections::NeoSection;
 use crate::snippets::Snippet;
@@ -5,16 +6,33 @@ use nom::branch::alt;
 use nom::bytes::complete::tag;
 use nom::bytes::complete::tag_no_case;
 use nom::bytes::complete::take_until;
+use nom::character::complete::line_ending;
 use nom::character::complete::multispace0;
 use nom::character::complete::newline;
 use nom::character::complete::not_line_ending;
 use nom::combinator::eof;
 use nom::combinator::rest;
+use nom::multi::many0;
 use nom::multi::many_till;
+use nom::sequence::delimited;
 use nom::sequence::terminated;
 use nom::sequence::tuple;
 use nom::IResult;
 use regex::Regex;
+
+pub fn attribute(source: &str) -> IResult<&str, Attribute> {
+    let (source, attr) = delimited(tag("-- accesskey: "), not_line_ending, line_ending)(source)?;
+    Ok((source, Attribute::AccessKey(attr.to_string())))
+}
+
+pub fn attributes(source: &str) -> IResult<&str, Option<Vec<Attribute>>> {
+    let (source, attributes) = many0(attribute)(source)?;
+    if attributes.len() == 0 {
+        Ok((source, None))
+    } else {
+        Ok((source, Some(attributes)))
+    }
+}
 
 pub fn aside(source: &str) -> IResult<&str, NeoSection> {
     let (source, _) = tuple((
@@ -23,11 +41,12 @@ pub fn aside(source: &str) -> IResult<&str, NeoSection> {
         not_line_ending,
         newline,
     ))(source)?;
+    let (source, attributes) = attributes(source)?;
     let (source, paragraphs) = many_till(paragraph, eof)(source)?;
     Ok((
         source,
         NeoSection::Aside {
-            attributes: None,
+            attributes,
             blocks: Some(paragraphs.0),
         },
     ))
@@ -45,132 +64,3 @@ pub fn paragraph(source: &str) -> IResult<&str, Block> {
         },
     ))
 }
-
-// pub fn block_end(source: &str) -> IResult<&str, &str> {
-//     let (source, remainder) = eof(source)?;
-//     Ok((source, remainder))
-// }
-
-// pub fn section_end(source: &str) -> IResult<&str, &str> {
-//     let (source, remainder) = eof(source)?;
-//     Ok((source, remainder))
-// }
-
-// pub fn paragraphs(source: &str) -> IResult<&str, Option<Vec<Block>>> {
-//     dbg!("77777777777777");
-//     let (source, _) = multispace0(source)?;
-//     dbg!("88888888888888");
-//     let (source, graphs) = opt(many_till(paragraph, eof))(source)?;
-//     dbg!("99999999999999");
-//     dbg!(&graphs);
-//     Ok((source, Some(graphs.unwrap().0)))
-// }
-
-// pub fn snippet(source: &str) -> IResult<&str, Snippet> {
-//     dbg!(&source);
-//     let (source, text) = many_till(
-//         anychar.map(|x| if x == '\n' { ' ' } else { x }),
-//         alt((tuple((line_ending, line_ending)).map(|_| ""), eof)),
-//     )(source)?;
-//     let response1 = &text.0.clone().into_iter().collect::<String>();
-//     dbg!(&response1);
-//     Ok((
-//         source,
-//         Snippet::Text {
-//             text: response1.to_string(),
-//         },
-//     ))
-// }
-
-// pub fn block_separator(source: &str) -> IResult<&str, Snippet> {
-//     let (source, _) = alt((tuple((line_ending, line_ending)).map(|_| ""), eof))(source)?;
-//     dbg!(&response1);
-//     Ok((
-//         source,
-//         Snippet::Text {
-//             text: response1.to_string(),
-//         },
-//     ))
-// }
-
-// pub fn paragraph(source: &str) -> IResult<&str, Block> {
-//     let (source, _) = multispace0(source)?;
-//     dbg!("00000000000000");
-//     let (source, snippets) = many_till(
-//         snippet,
-//         alt((tuple((line_ending, line_ending)).map(|_| ""), eof)),
-//     )(source)?;
-//     dbg!("22222222222222");
-//     dbg!(&snippets);
-//     dbg!(&source);
-//     Ok((
-//         source,
-//         Block::Paragraph {
-//             snippets: snippets.0,
-//         },
-//     ))
-//     // let (source, snippets) = many_till(
-//     //     snippet,
-//     //     alt((
-//     //         tuple((space0, crlf, space0, crlf)).map(|_| ""),
-//     //         pair(multispace0, eof).map(|_| ""),
-//     //         rest,
-//     //     )),
-//     // )(source)?;
-//     // dbg!("44444444444");
-//     // dbg!(&snippets);
-//     // dbg!(&source);
-//     // Ok((
-//     //     source,
-//     //     // Block::Paragraph {
-//     //     //     snippets: snippets.0,
-//     //     // },
-//     //     Block::None,
-//     // ))
-// }
-
-// pub fn snippets(source: &str) -> IResult<&str, Snippet> {
-//     let (source, _) = multispace0(source)?;
-//     Ok((source, Snippet::None))
-// }
-
-// pub fn snippet(source: &str) -> IResult<&str, Snippet> {
-//     dbg!("33333333333");
-//     let (source, _) = multispace0(source)?;
-//     let (source, value) = many_till(
-//         alt((
-//             is_not("\n\r\t"),
-//             pair(line_ending, not(crlf)).map(|_| " "),
-//             rest,
-//         )),
-//         alt((
-//             eof.map(|_| ""),
-//             tuple((line_ending, space0, line_ending)).map(|_| ""),
-//             eof,
-//         )),
-//     )(source)?;
-//     dbg!(&value);
-//     dbg!(&source);
-//     Ok((
-//         source,
-//         Snippet::Text {
-//             text: value.0.join(""),
-//             // text: "asdfasdfasdfasdfdadfs".to_string(),
-//         },
-//     ))
-// }
-
-// pub fn word(source: &str) -> IResult<&str, &str> {
-//     let (source, word) = is_not(" \n\r\t")(source)?;
-//     Ok((source, word))
-// }
-
-// pub fn not_separator(source: &str) -> IResult<&str, &str> {
-//     let (source, _) =
-//     Ok((source, ""))
-// }
-
-// pub fn separator(source: &str) -> IResult<&str, Snippet> {
-//     let
-
-// }
