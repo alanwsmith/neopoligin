463d1b63b21abcb4c2be200ea5b104be1b0942ec
Alan Smith <commit@alanwsmith.com>
2023-07-20 21:03:35 -0400

    getting list to work with multiple paragraphs

--- src/containers.rs
+++ src/containers.rs
@@ -1,10 +1,12 @@
 use crate::blocks::paragraph;
+// use crate::blocks::paragraphs;
 use crate::blocks::Block;
 use nom::bytes::complete::tag;
 use nom::multi::many1;
 use nom::sequence::preceded;
 use nom::IResult;
 use serde::{Deserialize, Serialize};
+use nom::character::complete::multispace0;
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 #[serde(tag = "type", rename_all = "lowercase")]
@@ -14,6 +16,20 @@ pub enum Container {
 }
 
 pub fn list_item(source: &str) -> IResult<&str, Container> {
-    let (source, content) = many1(preceded(tag("- "), paragraph))(source)?;
+    // let (_, content) = many1(preceded(tag("- "), paragraphs))(source)?;
+    // dbg!(&content);
+    let (source, content) =preceded(tag("- "), many1(preceded(multispace0, paragraph)))(source)?;
     Ok((source, Container::ListItem { content }))
 }
+
+
+// pub fn paragraphsx(source: &str) -> IResult<&str, Vec<Block>> {
+//     let (source, paragraphs) = many1(preceded(multispace0, paragraph))(source)?;
+//     Ok((source, paragraphs))
+// }
+
+// pub fn paragraph(source: &str) -> IResult<&str, Block> {
+//     let (source, _) = not(tag("--"))(source)?;
+//     let (source, snippets) = many1(preceded(opt(line_ending), alt((text, strong))))(source)?;
+//     Ok((source, Block::Paragraph { snippets }))
+// }
