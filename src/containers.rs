a49e9fb73168537cd84cd870dc4f8e9401763c04
Alan Smith <commit@alanwsmith.com>
2023-07-20 21:05:51 -0400

    refined list pull

--- src/containers.rs
+++ src/containers.rs
@@ -1,5 +1,4 @@
 use crate::blocks::paragraph;
-// use crate::blocks::paragraphs;
 use crate::blocks::Block;
 use nom::bytes::complete::tag;
 use nom::multi::many1;
@@ -16,20 +15,8 @@ pub enum Container {
 }
 
 pub fn list_item(source: &str) -> IResult<&str, Container> {
-    // let (_, content) = many1(preceded(tag("- "), paragraphs))(source)?;
-    // dbg!(&content);
-    let (source, content) =preceded(tag("- "), many1(preceded(multispace0, paragraph)))(source)?;
+    let (source, _) = tag("- ")(source)?;
+    let (source, content) = many1(preceded(multispace0, paragraph))(source)?;
     Ok((source, Container::ListItem { content }))
 }
 
-
-// pub fn paragraphsx(source: &str) -> IResult<&str, Vec<Block>> {
-//     let (source, paragraphs) = many1(preceded(multispace0, paragraph))(source)?;
-//     Ok((source, paragraphs))
-// }
-
-// pub fn paragraph(source: &str) -> IResult<&str, Block> {
-//     let (source, _) = not(tag("--"))(source)?;
-//     let (source, snippets) = many1(preceded(opt(line_ending), alt((text, strong))))(source)?;
-//     Ok((source, Block::Paragraph { snippets }))
-// }
