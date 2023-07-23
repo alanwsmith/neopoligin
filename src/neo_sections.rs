4b29440e4fdb876a7c905fa33e84085c8f406672
Alan Smith <commit@alanwsmith.com>
2023-07-23 14:10:06 -0400

    > put headers in place (only tested one

--- src/neo_sections.rs
+++ src/neo_sections.rs
@@ -35,12 +35,24 @@ use std::path::PathBuf;
 use crate::blocks::Block;
 use crate::containers::Container;
 use crate::neo_sections::aside_section::aside_section;
+use crate::neo_sections::h1_section::h1_section;
+use crate::neo_sections::h2_section::h2_section;
+use crate::neo_sections::h3_section::h3_section;
+use crate::neo_sections::h4_section::h4_section;
+use crate::neo_sections::h5_section::h5_section;
+use crate::neo_sections::h6_section::h6_section;
 use crate::neo_sections::image_section::image_section;
 use crate::neo_sections::list_section::list_section;
 use crate::neo_sections::p_section::p_section;
 use crate::neo_sections::title_section::title_section;
 
 pub mod aside_section;
+pub mod h1_section;
+pub mod h2_section;
+pub mod h3_section;
+pub mod h4_section;
+pub mod h5_section;
+pub mod h6_section;
 pub mod image_section;
 pub mod list_section;
 pub mod p_section;
@@ -52,6 +64,36 @@ pub enum NeoSection {
     Aside {
         attributes: Option<AttributesObj>,
         content: Option<Vec<Block>>,
+    },
+    H1 {
+        attributes: Option<AttributesObj>,
+        body: Option<Vec<Block>>,
+        headline: Option<Block>,
+    },
+    H2 {
+        attributes: Option<AttributesObj>,
+        body: Option<Vec<Block>>,
+        headline: Option<Block>,
+    },
+    H3 {
+        attributes: Option<AttributesObj>,
+        body: Option<Vec<Block>>,
+        headline: Option<Block>,
+    },
+    H4 {
+        attributes: Option<AttributesObj>,
+        body: Option<Vec<Block>>,
+        headline: Option<Block>,
+    },
+    H5 {
+        attributes: Option<AttributesObj>,
+        body: Option<Vec<Block>>,
+        headline: Option<Block>,
+    },
+    H6 {
+        attributes: Option<AttributesObj>,
+        body: Option<Vec<Block>>,
+        headline: Option<Block>,
     },
     Image {
         attributes: Option<AttributesObj>,
@@ -79,6 +121,12 @@ pub fn neo_section(source: &str) -> IResult<&str, NeoSection, VerboseError<&str>
     let (source, section) = alt((
         aside_section,
         image_section,
+        h1_section,
+        h2_section,
+        h3_section,
+        h4_section,
+        h5_section,
+        h6_section,
         list_section,
         p_section,
         title_section,
