6dd5798bdcbcc55f5958be84178bdd7992955e6f
Alan Smith <commit@alanwsmith.com>
2023-08-01 21:53:02 -0400

    - get list items working, i think

--- src/blocks/paragraph_block.rs
+++ src/blocks/paragraph_block.rs
@@ -1,10 +1,19 @@
 use crate::blocks::Block;
 use crate::tokens::token;
+// use nom::character::complete::multispace0;
 use nom::error::VerboseError;
 use nom::multi::many1;
 use nom::IResult;
+use nom::combinator::not;
+use nom::bytes::complete::tag;
 
 pub fn paragraph_block(source: &str) -> IResult<&str, Block, VerboseError<&str>> {
+    // dbg!(&source);
+    // let (source, _) = multispace0(source)?;
+    // dbg!(&source);
+    let (source, _) = not(tag("-"))(source)?;
     let (source, content) = many1(token)(source)?;
+    // dbg!(&content);
+    // dbg!(&source);
     Ok((source, Block::Paragraph { content }))
 }
--- src/containers/list_item_container.rs
+++ src/containers/list_item_container.rs
@@ -5,10 +5,17 @@ use nom::error::VerboseError;
 use nom::multi::many1;
 use nom::IResult;
 use nom::character::complete::multispace0;
+use nom::character::complete::line_ending;
+use nom::sequence::terminated;
 
 pub fn list_item_container(source: &str) -> IResult<&str, Container, VerboseError<&str>> {
+    // dbg!(&source);
     let (source, _) = multispace0(source)?;
+    // dbg!(&source);
     let (source, _) = tag("- ")(source)?;
-    let (source, body) = many1(paragraph_block)(source)?;
+    // dbg!(&source);
+    let (source, body) = many1(terminated(paragraph_block, line_ending))(source)?;
+    // dbg!(&body);
+    // dbg!(&source);
     Ok((source, Container::ListItem { body }))
 }
