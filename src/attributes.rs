e56da4a79d720b7a90d0ace42cd7ef3b4245ffd6
Alan Smith <commit@alanwsmith.com>
2023-08-03 00:51:44 -0400

    - cleanup attributes

--- src/attributes.rs
+++ src/attributes.rs
@@ -1,13 +1,6 @@
-#![allow(unused_imports)]
-use crate::attributes::accesskey_attribute::accesskey_attribute;
-use crate::attributes::autocapitalize_attribute::autocapitalize_attribute;
-use crate::attributes::autofocus_attribute::autofocus_attribute;
-use crate::attributes::class_attribute::class_attribute;
-use crate::attributes::contenteditable_attribute::contenteditable_attribute;
-use crate::attributes::id_attribute::id_attribute;
-use crate::attributes::type_attribute::type_attribute;
 use nom::branch::alt;
 use nom::bytes::complete::tag;
+use nom::bytes::complete::tag_no_case;
 use nom::combinator::opt;
 use nom::error::VerboseError;
 use nom::multi::many1;
@@ -18,39 +11,7 @@ use nom::bytes::complete::is_not;
 use nom::character::complete::line_ending;
 use nom::character::complete::space0;
 use nom::sequence::terminated;
-
-pub mod accesskey_attribute;
-pub mod autocapitalize_attribute;
-pub mod autofocus_attribute;
-pub mod class_attribute;
-pub mod contenteditable_attribute;
-pub mod id_attribute;
-pub mod type_attribute;
-
-// TODO: Move away from AttributesObj
-// to AttrubteList
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
-pub struct AttributesObj {
-    accesskey: Option<String>,
-    autocapitalize: Option<String>,
-    autofocus: Option<bool>,
-    contenteditable: Option<String>,
-    class: Option<Vec<String>>,
-    id: Option<String>,
-}
-
-impl AttributesObj {
-    pub fn new() -> AttributesObj {
-        AttributesObj {
-            accesskey: None,
-            autocapitalize: None,
-            autofocus: None,
-            contenteditable: None,
-            class: None,
-            id: None,
-        }
-    }
-}
+use nom::Parser;
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 #[serde(tag = "key", content = "value", rename_all = "lowercase")]
@@ -69,9 +30,7 @@ pub enum AttributeV2 {
     None,
 }
 
-
 pub fn attribute(source: &str) -> IResult<&str, AttributeV2, VerboseError<&str>> {
-    // dbg!(source);
     let (source, attrs) = preceded(
         alt((tag("--"), tag("|"))),
         alt((
@@ -93,6 +52,58 @@ pub fn attribute(source: &str) -> IResult<&str, AttributeV2, VerboseError<&str>>
 }
 
 
+
+pub fn accesskey_attribute(source: &str) -> IResult<&str, AttributeV2, VerboseError<&str>> {
+    let (source, _) = space0(source)?;
+    let (source, attr) = preceded(tag("accesskey: "), is_not("|>\n"))(source)?;
+    let (source, _) = opt(line_ending)(source)?;
+    Ok((source, AttributeV2::AccessKey(attr.to_string())))
+}
+
+pub fn autocapitalize_attribute(source: &str) -> IResult<&str, AttributeV2, VerboseError<&str>> {
+    // autocapitzlize has a specific set of options but the
+    // ROI of limiting to them isn't high for phase 1 so
+    // just passing in whatever string. Something to look into
+    // for a future iteration maybe
+    let (source, _) = space0(source)?;
+    let (source, attr) = preceded(tag("autocapitalize: "), is_not("|>\n"))(source)?;
+    let (source, _) = opt(line_ending)(source)?;
+    Ok((source, AttributeV2::AutoCapitalize(attr.to_string())))
+}
+
+pub fn autofocus_attribute(source: &str) -> IResult<&str, AttributeV2, VerboseError<&str>> {
+    let (source, _) = space0(source)?;
+    let (source, _attr) = tag_no_case("autofocus")(source)?;
+    let (source, _) = opt(line_ending)(source)?;
+    Ok((source, AttributeV2::AutoFocus))
+}
+
+
+
+
+
+
+pub fn class_attribute(source: &str) -> IResult<&str, AttributeV2, VerboseError<&str>> {
+    let (source, _) = space0(source)?;
+    let (source, _attr) = tag("class:")(source)?;
+    let (source, value) = many1(preceded(
+        space0,
+        is_not(" |>\n").map(|c: &str| c.to_string()),
+    ))(source)?;
+    let (source, _) = opt(line_ending)(source)?;
+    Ok((source, AttributeV2::Class(value)))
+}
+
+pub fn contenteditable_attribute(source: &str) -> IResult<&str, AttributeV2, VerboseError<&str>> {
+    let (source, _) = space0(source)?;
+    let (source, value) = preceded(tag("contenteditable: "), is_not("|>\n"))(source)?;
+    let (source, _) = opt(line_ending)(source)?;
+    Ok((source, AttributeV2::ContentEditable(value.to_string())))
+}
+
+
+
+
 pub fn generic_attribute(source: &str) -> IResult<&str, AttributeV2, VerboseError<&str>> {
     let (source, _) = space0(source)?;
     let (source, key) = terminated(is_not(":"), tag(":"))(source)?;
@@ -101,6 +112,13 @@ pub fn generic_attribute(source: &str) -> IResult<&str, AttributeV2, VerboseErro
     let (source, _) = space0(source)?;
     let (source, _) = opt(line_ending)(source)?;
     Ok((source, AttributeV2::Generic((key.to_string(), value.to_string()))))
+}
+
+pub fn id_attribute(source: &str) -> IResult<&str, AttributeV2, VerboseError<&str>> {
+    let (source, _) = space0(source)?;
+    let (source, attr) = preceded(tag("id: "), is_not("|>\n"))(source)?;
+    let (source, _) = opt(line_ending)(source)?;
+    Ok((source, AttributeV2::Id(attr.to_string())))
 }
 
 
@@ -119,6 +137,16 @@ pub fn title_attribute(source: &str) -> IResult<&str, AttributeV2, VerboseError<
     let (source, _) = opt(line_ending)(source)?;
     Ok((source, AttributeV2::Title(attr.to_string())))
 }
+
+
+pub fn type_attribute(source: &str) -> IResult<&str, AttributeV2, VerboseError<&str>> {
+    let (source, _) = space0(source)?;
+    let (source, attr) = preceded(tag("type: "), is_not("|>\n"))(source)?;
+    let (source, _) = opt(line_ending)(source)?;
+    Ok((source, AttributeV2::Type(attr.to_string())))
+}
+
+
 
 pub fn url_attribute(source: &str) -> IResult<&str, AttributeV2, VerboseError<&str>> {
     let (source, _) = space0(source)?;
