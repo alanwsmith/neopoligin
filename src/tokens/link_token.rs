03d9a97d14b4ff2e630ad0f2e30a801ab08799e2
Alan Smith <commit@alanwsmith.com>
2023-08-07 14:12:24 -0400

    Made links multiline

--- src/tokens/link_token.rs
+++ src/tokens/link_token.rs
@@ -3,13 +3,24 @@ use nom::bytes::complete::is_not;
 use nom::bytes::complete::tag;
 use nom::error::VerboseError;
 use nom::IResult;
+use nom::character::complete::space0;
 
 pub fn link_token(source: &str) -> IResult<&str, Token, VerboseError<&str>> {
-    let (source, _) = tag("<<link|")(source)?;
-    let (source, string) = is_not("|")(source)?;
+    dbg!(&source);
+    let (source, _) = tag("<<link")(source)?;
+    dbg!(&source);
+    let (source, _) = space0(source)?;
+    dbg!(&source);
     let (source, _) = tag("|")(source)?;
+    dbg!(&source);
+    let (source, string) = is_not("|")(source)?;
+    dbg!(&source);
+    let (source, _) = tag("|")(source)?;
+    dbg!(&source);
     let (source, url) = is_not(">")(source)?;
+    dbg!(&source);
     let (source, _) = tag(">>")(source)?;
+    dbg!(&source);
     Ok((
         source,
         Token::Link {
