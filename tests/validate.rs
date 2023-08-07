9335f8a008059350f8f5334feaa65c2f71f2b1d7
Alan Smith <commit@alanwsmith.com>
2023-08-07 13:35:40 -0400

    - starting to add tests for tokens

--- tests/validate.rs
+++ tests/validate.rs
@@ -20,8 +20,34 @@ struct TestShell {
 }
 
 #[test]
-fn solo_test_specs() {
+fn sections_test() {
     let paths = fs::read_dir("./spec/sections").unwrap();
+    for path in paths {
+        let file_path = path.unwrap().path().display().to_string();
+        if file_path.find(".json") != None {
+            let json_text = fs::read_to_string(file_path).unwrap();
+            let test_shell: TestShell = serde_json::from_str(json_text.as_str()).unwrap();
+            // Run Solo Tests First
+            test_shell.tests.into_iter().for_each(|test| {
+                if let Some(_) = test.solo {
+                    let expected = test.clone().expected;
+                    let mut p = Page::new_from(&test.input);
+                    let results = p.raw_sections();
+                    assert_eq!(expected, results);
+                }
+                // Then run everything
+                let expected = test.expected;
+                let mut p = Page::new_from(&test.input);
+                let results = p.raw_sections();
+                assert_eq!(expected, results);
+            });
+        }
+    }
+}
+
+#[test]
+fn tokens_test() {
+    let paths = fs::read_dir("./spec/tokens").unwrap();
     for path in paths {
         let file_path = path.unwrap().path().display().to_string();
         if file_path.find(".json") != None {
